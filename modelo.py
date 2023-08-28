a2ec833844454117de70be8d4668a791041f1c47
HisaeNakagima <1823289@aluno.univesp.br>
2023-08-28 09:29:50 -0300

    Removendo duplicação

--- modelo.py
+++ modelo.py
@@ -1,55 +1,46 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
-    @property
-    def likes(self):
-         return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def likes(self):
-         return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+        self._likes = 0
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
+vingadores.dar_likes()
 print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
 f'- Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
-atlanta.dar_like()
-atlanta.dar_like()
+atlanta.dar_likes()
+atlanta.dar_likes()
 print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
 f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
 
